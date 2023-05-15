3c984b4e16d441e71a3df6a2c8059c07163e75a8
Sergey Kuznetsov <kuuuzya@ya.ru>
2023-05-15 12:48:26 +0400

    Added code to download files to folder

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import os
+from tqdm import tqdm
 import re
-from os.path import exists
-import shutil
-import string
-
-import re
+from git import Repo
 
 # Открываем файл для чтения
-with open('filename.txt', 'r') as file:
+with open('source.txt', 'r') as file:
     text = file.read()
 
 # Применяем регулярное выражение для очистки данных
 clean_text = re.sub(r'^(?!sd-concepts-library/.*$).*', '', text, flags=re.MULTILINE)
+clean_text = re.sub(r'\n\s*\n', '\n', clean_text)
+clean_text = clean_text.rstrip('\n')
 
+new_text = '\n'.join(['git clone https://huggingface.co/' + line for line in clean_text.split('\n')])
 
-in_file="/sd/con/in.txt"
-out_file="d:\\sd-con\\clone_repos.bat"
-with open(in_file) as f:
-    lines = f.readlines()
-    for line in lines:
-        print(line)
-        line = re.sub(r'^(?!sd-concepts-library/.*$).*', '\n', line)
-        line = line.replace('sd-concepts-library/', 'git clone https://huggingface.co/sd-concepts-library/')
-        line = line.replace("\n\n\n","")
-        line = line.replace("\n\n","")
-        line = line.replace("\n\n\n\n","")
-        with(open(out_file, 'a')) as t_f:
-            t_f.write(line)
+links = new_text.splitlines()
+links = [s.replace('git clone ', '') for s in links]
+print(links)
+
+if not os.path.exists('downloads'):
+    os.makedirs('downloads')
+
+# проходимся по ссылкам и клонируем репозитории
+for link in tqdm(links):
+    # извлекаем имя папки из ссылки
+    foldername = link.split('/')[-1].rstrip('.git')
+    # создаем путь для сохранения папки
+    path = os.path.join('downloads', foldername)
+    # клонируем репозиторий по ссылке
+    Repo.clone_from(link, path)
